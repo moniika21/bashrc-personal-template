c46072869043f5a5dbc2556b98b2db20d680430c
moniika21 <evancutaia21@gmail.com>
2022-11-13 15:14:32 +0100

    added  some comments

--- custom-aliases.zsh
+++ custom-aliases.zsh
@@ -7,7 +7,7 @@ then
     alias update="sudo nala update"
     alias upgrade="sudo nala upgrade"
     alias autoremove="sudo nala autoremove"
-    alias maj="upgrade -y && autoremove -y" # upgrade do a update automatically before upgrading 
+    alias maj="upgrade -y && autoremove -y" # upgrade command do a update automatically before upgrading 
 else
     alias dl="sudo apt -qq update && echo 'n' | sudo apt upgrade 2>/dev/null | sed -n '/^Need to get/p'"
     alias install="sudo apt install"
